ed47254fc1491dbfd2ec0a9f01ce5b35cbdd6881
mohammadreza <spd.mohammadreza@gmail.com>
2023-10-30 01:45:13 -0700

    start portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className=''>mohammadreza</div>
+    </>
+  )
+}
+
+export default App
