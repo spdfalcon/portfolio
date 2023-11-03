d4438f1f538381cf7321d12020692c6d1b126feb
mohammadreza <spd.mohammadreza@gmail.com>
2023-11-03 08:51:40 -0700

    mohammad

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import StartHeader from '../../components/startHeader/StartHeader'
 import Projects from '../../components/Projects/Projects'
 import ViewAllProjects from '../../components/viewAllProjects/ViewAllProjects'
-import Frameworks from '../../components/Frameworks/Frameworks'
 import TeamPic from '../../components/TeamPic/TeamPic'
 import StartProject from '../../components/StartProject/StartProject'
+import Frameworks from '../../components/frameworks/frameworks'
 
 export default function Home() {
   return (
