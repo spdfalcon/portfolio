318fee4543b946bf5260419eb93d55374b8901e2
mohammadreza <spd.mohammadreza@gmail.com>
2023-11-03 07:45:16 -0700

    mohammad

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,13 @@
 import Home from "../pages/Home/Home"
 import AboutMe from "../pages/About Me/AboutMe"
 import Work from "../pages/Work/Work"
+import SingleWork from "../pages/SingleWork/SingleWork"
 
 const routes = [
 {path:'/' , element:<Home></Home>},
 {path:'/about-me' , element:<AboutMe></AboutMe>},
-{path:'/work' , element:<Work></Work>}
+{path:'/work' , element:<Work></Work>},
+{path:'/work/:workID' , element:<SingleWork></SingleWork> }
 ]
 
 
