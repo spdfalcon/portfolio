bfdeac67f1df4dea4ff1b7bab3447a84df5f730f
mohammadreza <spd.mohammadreza@gmail.com>
2023-11-09 12:42:55 -0800

    responsiv

--- src/components/SingleWorkProjectBrief/SingleWorkProjectBrief.jsx
+++ src/components/SingleWorkProjectBrief/SingleWorkProjectBrief.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function SingleWorkProjectBrief() {
   return (
     <>
-      <div className="container grid grid-cols-1 md:grid-cols-2 mt-24">
+      <div className="container grid grid-cols-1 lg:grid-cols-2 mt-24 gap-y-20 lg:gap-y-0">
         <div className="left">
             <div className="top">
                 <Link to={''} className="py-[10px] px-[14px] bg-[#f4f8ff] rounded-md text-sm ">Our Work</Link>
@@ -18,8 +18,8 @@ export default function SingleWorkProjectBrief() {
                 <h3 className="font-inter600 text-work-text">Development</h3>
             </div>
         </div>
-        <div className="flex justify-center">
-        <div className="right w-[487px] ">
+        <div className="lg:flex lg:justify-center">
+        <div className="right max-w-[487px] ">
             <div className="up text-work-text-gray">
             <p>Our job is to build your website so that it is functional and user friendly but at the same time attractive. My aim is to bring across your message and identity in the most creative way.</p>
             <p className="mt-6">I enjoy turning complex problems into simple, beautiful and intuitive interface designs. When I’m not coding or pushing pixels, you’ll find me in the pool or on the court shooting hoops.</p>
