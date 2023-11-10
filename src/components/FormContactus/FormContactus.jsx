1b3bc4a9704a47a90aeb660c9bfc46d81a26d688
mohammadreza <spd.mohammadreza@gmail.com>
2023-11-10 13:22:33 -0800

    single page

--- src/components/FormContactus/FormContactus.jsx
+++ src/components/FormContactus/FormContactus.jsx
@@ -19,7 +19,7 @@ export default function FormContactus() {
             </div>
             <div className="flex gap-2 border-b-2 px-4 py-2 placeholder:text-work-text-gray text-work-text-gray">
               <label htmlFor="Name">
-              <i class="bi bi-envelope"></i>
+                <i class="bi bi-envelope"></i>
               </label>
               <input
                 className="outline-none"
@@ -30,7 +30,7 @@ export default function FormContactus() {
             </div>
             <div className="flex gap-2 border-b-2 px-4 py-2 placeholder:text-work-text-gray text-work-text-gray">
               <label htmlFor="Name">
-              <i class="bi bi-telephone"></i>
+                <i class="bi bi-telephone"></i>
               </label>
               <input
                 className="outline-none"
@@ -40,14 +40,37 @@ export default function FormContactus() {
               />
             </div>
             <div className="flex gap-2 border-b-2 px-4 py-2 placeholder:text-work-text-gray text-work-text-gray">
-              <select className="w-full text-work-text-gray outline-none"  name="" id="">
-                <option className="text-work-text-gray" value="Subject">Subject</option>
+              <select
+                className="w-full text-work-text-gray outline-none"
+                name=""
+                id=""
+              >
+                <option className="text-work-text-gray" value="Subject">
+                  Subject
+                </option>
               </select>
             </div>
-            <div className="">
-
+            <div className=" col-span-2 flex gap-2 border-b-2 px-4 py-2 placeholder:text-work-text-gray text-work-text-gray h-40">
+              <i class="bi bi-pencil"></i>
+              <textarea
+                name=""
+                id=""
+                className="w-full outline-none "
+                placeholder="How can we help you ? Feel free to get in touch!"
+              ></textarea>
             </div>
+            <div className="flex gap-2 items-center text-work-text-gray col-span-2">
+              <div className="w-5 h-5 border rounded-sm"></div>
+              <h4 className="">I agree that my data is collected and stored</h4>
+            </div>
+            <a
+              className=" mt-5 px-6 py-3 bg-black-me text-white-me rounded-lg w-fit"
+              href="#"
+            >
+              🤙 Get In Touch
+            </a>
           </div>
+          
         </form>
       </div>
     </>
--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -19,6 +19,15 @@ export default function ContactUs() {
         <AboutMeQuestion></AboutMeQuestion>
         <FormContactus></FormContactus>
         </div>
+        <div className="mt-20">
+        <iframe
+          className="rounded-xl w-full h-[650px]"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52993.009607584805!2d48.79491879935407!3d33.88802869575732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fec2860fa91d733%3A0x551664c49aac52d3!2z2KjYsdmI2KzYsdiv2Iwg2KfYs9iq2KfZhiDZhNix2LPYqtin2YbYjCDYp9uM2LHYp9mG!5e0!3m2!1sfa!2sde!4v1699651000963!5m2!1sfa!2sde"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
     </>
   );
