f3e9fc60b84099d255f3be29bc4d6d41fc0eafca
mohammadreza <spd.mohammadreza@gmail.com>
2023-11-10 11:42:08 -0800

    single page

---
+++ src/components/AboutmeHeader/AboutmeHeader.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import CounterHome from "../CounterHome/CounterHome";
+
+export default function AboutmeHeader() {
+  return (
+    <>
+      <div>
+        <div className="top text-center py-12">
+          <h1 className="text-3xl lg:text-5xl font-inter600 text-work-text">
+            About Me
+          </h1>
+          <div className="flex justify-center mt-5">
+            <p className="max-w-[607px] text-work-text-gray">
+              12 — SaaS companies we’ve helped in the past 24 months. Our
+              holistic process takes care of discovery, branding, design, and
+              development.
+            </p>
+          </div>
+          <div className="mt-5">
+          <i class="bi bi-arrow-down"></i>
+          </div>
+        </div>
+        <div className="down grid grid-cols-1 lg:grid-cols-2">
+          <div className="left flex justify-center">
+            <img src="/img/uiux/uiux.png" alt="" />
+          </div>
+          <div className=" right mt-5 lg:mt-0">
+            <h2 className="text-3xl lg:text-5xl font-inter600 text-work-text">
+              Infuse Your Brand with Excellence
+            </h2>
+            <p className="mt-5 max-w-[607px] text-work-text-gray">
+              Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit.
+            </p>
+            <div className="mt-5 flex flex-col gap-2">
+              <div className="flex gap-2">
+                <i class="bi bi-check-circle"></i>
+                <h4>Adipiscing eli sed eiusmod</h4>
+              </div>
+              <div className="flex gap-2">
+                <i class="bi bi-check-circle"></i>
+                <h4>Tempor incididunt</h4>
+              </div>
+              <div className="flex gap-2">
+                <i class="bi bi-check-circle"></i>
+                <h4>Labore et dolore magna</h4>
+              </div>
+              <button className="text-xl px-5 py-4 bg-gradient-to-r from-gradient-boxB1 via-gradient-headerB2 to-gradient-boxB3 w-fit mt-16 rounded-2xl font-inter600 ">🦾 Contact Us</button>
+            </div>
+          </div>
+        </div>
+        <div className="numbers py-12 flex justify-between items-center">
+            <CounterHome title={'Project'} count={98}></CounterHome>
+            <span className="w-px h-10 lg:h-16 bg-[#dee2d2]"></span>
+            <CounterHome title={'People'} count={65}></CounterHome>
+            <span className="w-px h-10 lg:h-16 bg-[#dee2d2]"></span>
+            <CounterHome title={'Years'} count={10}></CounterHome>
+            <span className="w-px h-10 lg:h-16 bg-[#dee2d2]"></span>
+            <CounterHome title={'Offices'} count={15}></CounterHome>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/About Me/AboutMe.jsx
+++ src/pages/About Me/AboutMe.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import AboutmeHeader from "../../components/AboutmeHeader/AboutmeHeader";
+import TeamPic from "../../components/TeamPic/TeamPic";
+import SingleWorkSlider from "../../components/SingleWorkSlider/SingleWorkSlider";
+import SingleWorkTestimonials from "../../components/SingleWorkTestimonials/SingleWorkTestimonials";
+import StartProject from "../../components/StartProject/StartProject";
 
 export default function AboutMe() {
   return (
     <div>
-      About me
+      <div className="container">
+        <AboutmeHeader></AboutmeHeader>
+      </div>
+      <TeamPic></TeamPic>
+      <div className="container">
+        <div className="title text-center w-full py-5"></div>
+        <div className="all flex flex-col items-center gap-5 lg:flex-row py-28">
+          <div className="left relative basis-1/2">
+            <div className="hidden lg:block">
+              <img src="/img/uiux/uiux.png" alt="" />
+            </div>
+            <div className="lg:absolute w-80 h-48 top-72 left-32 bg-black-me p-4 text-white-me">
+              <span className="text-[13px]">01</span>
+              <h3 className="text-2xl font-inter600">Client Brief</h3>
+              <p className="text-sm mt-4">
+                Expedita laborum suscipit sequi et nobis voluptas fuga placeat.
+                Omnis est ratione. Repellendus recusandae a.
+              </p>
+            </div>
+          </div>
+          <div className="right basis-1/2">
+            <h4 className="text-gray-text2 ">HOW WE WORK</h4>
+            <h2 className="text-2xl lg:text-5xl mt-5 text-work-text font-inter600">
+              Our Design Process in Four Steps
+            </h2>
+            <p className="text-gray-text2 mt-5">
+              Adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+            </p>
+            <div className="mt-5">
+              <h2 className="text-[26px] font-inter600">01. Client Brief</h2>
+              <div className="text-[26px] text-gray-text3">
+                <h4>02. Analytics and Prototyping</h4>
+                <h4>03. Design</h4>
+                <h4>04. Launching and Support</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src="/img/Aboutme/1.png" alt="" />
+      </div>
+      <SingleWorkTestimonials></SingleWorkTestimonials>
+      <StartProject></StartProject>
     </div>
-  )
+  );
 }
