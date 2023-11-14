8d65b7ef4f5904402bb4e6035f69ed947f315a74
mohammadreza <spd.mohammadreza@gmail.com>
2023-11-14 06:32:09 -0800

    vercel back

--- src/Services/Axios/configs.js
+++ src/Services/Axios/configs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+// http://localhost:3000
 const apiRequests = axios.create({
-    baseURL:'http://localhost:3000',
+    baseURL:'https://portfolio-json-server-vercel.vercel.app',
     headers: {
         'Content-Type' : 'application/json',
     }
